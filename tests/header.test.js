61c954d5323bd65a7d334cc43223395f4a809f09
Shayne Meyer <shayne.meyer@providence.org>
2018-03-30 13:06:04 -0700

    added authflow test

--- tests/header.test.js
+++ tests/header.test.js
@@ -10,8 +10,20 @@ beforeEach(async () => {
     await page.goto('localhost:3000');
 });
 
-test('We can launch a browser', async () => {
+afterEach(async () => {
+    await browser.close();
+});
+
+test('the header has the correct text', async () => {
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
 
     expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oauth flow', async () =>{
+      await page.click('.right a');
+
+      const url = await page.url();
+
+      console.log(url);
 });
